e4d421462f1aa9658d1f8d892639ef725cb8c392
Sofia-Sikandreen <sofiasikandreen@gmail.com>
2026-05-14 14:57:02 +0500

    fix: upload

--- cms/src/collections/Media.ts
+++ cms/src/collections/Media.ts
@@ -2,12 +2,21 @@ import type { CollectionConfig } from "payload";
 
 export const Media: CollectionConfig = {
   slug: "media",
-  
+
   access: {
     read: () => true,
     create: () => true,
   },
 
-  upload: true,
+  upload: {
+    staticDir: "media",
+    mimeTypes: [
+      "image/png",
+      "image/jpeg",
+      "image/jpg",
+      "application/pdf",
+    ],
+  },
+
   fields: [],
 };
